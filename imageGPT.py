b7c40ac5a36f155610c98822786d1a3a4b04a201
Nardini <jaromaniello@gmail.com>
2023-06-29 21:02:55 -0300

    Update image func

--- imageGPT.py
+++ imageGPT.py
@@ -7,14 +7,18 @@ API_ENDPOINT = "https://api.openai.com/v1/images/generations"
 
 openai.api_key = variables.API_KEY
 
-response = openai.Image.create(
-  prompt="a white siamese cat",
-  n=1,
-  size="1024x1024"
-)
 
-image_url = response['data'][0]['url']
-response = requests.get(image_url)
+def generateImg(imageContext, options):
+    response = openai.Image.create(
+        prompt=imageContext,
+        n=options,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    response = requests.get(image_url)
 
-with open("imagem/image" + str(random.randint(0, 400)) + ".jpg", "wb") as f:
-    f.write(response.content)
+    with open("imagem/image" + str(random.randint(0, 400)) + ".jpg", "wb") as f:
+        f.write(response.content)
+
+
+generateImg("Uma cadela feliz descendo em um escorregador", options=1)
